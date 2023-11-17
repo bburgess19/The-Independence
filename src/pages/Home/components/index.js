2398363a83bede5b4fed15b397304d55b3949b63
bburgess19 <bburgess19@cs.uchicago.edu>
2023-11-16 21:44:20 -0600

    Add sub-gallery banner

--- src/pages/Home/components/index.js
+++ src/pages/Home/components/index.js
@@ -1,11 +1,17 @@
 import Gallery from './Gallery.js'
 import ArticleList from './ArticleList.js';
+import '../../../assets/Home.css'
 
 function Home() {
     return (
         <>
             <Gallery />
-            <ArticleList />
+            <div id="sub-gallery-banner">
+                <h2 id="sub-gallery-title">A Publication About Black Experiences</h2>
+            </div>
+            <div id="page-wrapper">
+                <ArticleList />
+            </div>
         </>
     )
 }
