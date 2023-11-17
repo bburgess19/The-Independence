0eb79b07f4fb49565dedf9d368821b4454f5216a
bburgess19 <bburgess19@cs.uchicago.edu>
2023-11-16 21:46:33 -0600

    Add routing to the expanded menu and a max content width of 1400 px

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -1,6 +1,7 @@
 import {Outlet} from "react-router-dom"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import "../../App.css"
 export default function Layout() {
     return (
         <>
