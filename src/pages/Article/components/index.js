852fc774db1eb7d9c8b929287a63bb5f6016f25c
Ben Burgess <bburgess19@cs.uchicago.edu>
2023-12-20 10:41:01 -0500

    Parse body from markdown
    I've decided to store articles as markdown in the public folder, and the
    loading process will entail:
    
    1. Fetch the metadata from the server
    2. Use the document ID to fetch the article markdown
    3. Parse the article markdown with `marked-react`
    
    Relatedly, the old `Body` component is deleted

--- src/pages/Article/components/index.js
+++ src/pages/Article/components/index.js
@@ -1,39 +1,66 @@
 import Body from "./Body";
 import TitleImage from "./TitleImage";
 import AuthorBlurb from "./AuthorBlurb";
-import {useEffect, useState} from "react";
-import {db} from '../../../config/firebase.js';
-import {collection, where, query, getDocs} from 'firebase/firestore';
-import {useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { db } from "../../../config/firebase.js";
+import { collection, where, query, getDocs } from "firebase/firestore";
+import { useParams } from "react-router-dom";
+import Markdown from "marked-react";
 
 export default function Article(props) {
-    const [article, setArticle] = useState(null);
-    const params = useParams();
+  const [article, setArticle] = useState({ data: null, id: null });
+  const [body, setBody] = useState(null);
+  const params = useParams();
 
-    useEffect(() => {
-        const getArticle = async () => {
-            try {
-                const queryConstraints = [];
-                queryConstraints.push(where('slug', '==', params.slug));
-                const q = query(collection(db, 'articles'), ...queryConstraints);
-                const snapshot = await getDocs(q);
-                setArticle(snapshot.docs[0].data());
-            } catch (e) {
-                console.log(e);
-                return (<h1>Page failed to load</h1>);
-            }
+  useEffect(() => {
+    const getArticle = async () => {
+      try {
+        const queryConstraints = [];
+        queryConstraints.push(where("slug", "==", params.slug));
+        const q = query(collection(db, "articles"), ...queryConstraints);
+        const snapshot = await getDocs(q);
+        setArticle({ data: snapshot.docs[0].data(), id: snapshot.docs[0].id });
+      } catch (e) {
+        console.log(e);
+        return <h1>Page failed to load</h1>;
+      }
+    };
+
+    getArticle();
+  }, [params]);
+
+  // Fetch the body when the article is loaded
+  useEffect(() => {
+    if (article.data !== null) {
+      // Send a http request to public/essays/id.md
+
+      const fetchBody = async () => {
+        try {
+          const response = await fetch("/essays/" + article.id + ".md");
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+
+          const text = await response.text();
+          setBody(text);
+        } catch (e) {
+          console.error("Could not fetch the body of the article");
         }
+      };
 
-        getArticle();
-    }, [params]);
+      fetchBody();
+    }
+  }, [article]);
 
-    if (article === null) return (<h1>Loading...</h1>);
-    return (
-        <>
-            <TitleImage article={article} />
-            <h2 id="subtitle">Here is a subtitle that's decently long</h2>
-            <Body />
-            <AuthorBlurb authorName={article.author} />
-        </>
-    )
+  if (article.data === null) return <h1>Loading...</h1>;
+  return (
+    <>
+      <TitleImage article={article.data} />
+      <div className="page-wrapper">
+        <h2 id="subtitle">Here is a subtitle that's decently long</h2>
+        <Markdown>{body}</Markdown>
+        <AuthorBlurb authorName={article.data.author} />
+      </div>
+    </>
+  );
 }
