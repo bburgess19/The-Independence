4a77e5c87e73bc475df58786b3c001caf796b7d7
Ben Burgess <bburgess19@cs.uchicago.edu>
2023-12-25 11:27:06 -0500

    Make article thumbnails scale with width

--- src/pages/Home/components/index.js
+++ src/pages/Home/components/index.js
@@ -1,19 +1,21 @@
-import Gallery from './Gallery.js'
-import ArticleList from './ArticleList.js';
-import '../../../assets/Home.css'
+import Gallery from "./Gallery.js";
+import ArticleList from "./ArticleList.js";
+import "../../../assets/Home.css";
 
 function Home() {
-    return (
-        <>
-            <Gallery />
-            <div id="sub-gallery-banner">
-                <h2 id="sub-gallery-title">A Publication About Black Experiences</h2>
-            </div>
-            <div className="page-wrapper">
-                <ArticleList />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Gallery />
+      <div id="sub-gallery-banner">
+        <div>
+          <h3 id="sub-gallery-title">A Publication About Black Experiences</h3>
+        </div>
+      </div>
+      <div className="page-wrapper">
+        <ArticleList />
+      </div>
+    </>
+  );
 }
 
 export default Home;
